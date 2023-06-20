84b9d100c84d31a896e219be1074ef11c095ed29
[Iman Jama] <[Iman.jama891@gmail.com]>
2023-06-20 18:29:31 +0100

    fixed db.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -29,10 +29,11 @@ export const getDb = async () =>{
   const editorDb = await openDB('jate', 1);
   const tx = editorDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
+  console.log(request);
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 initdb();
